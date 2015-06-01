f224c224661eeae780ad3247ba40158bbca6aa5c
s14003 <s14003@std.it-college.ac.jp>
2015-06-01 15:52:46 +0900

    calender

--- calender.js
+++ calender.js
@@ -1,29 +1,34 @@
 function converCalender(year) {
 
 }
+
 function convertWarekiToSeireki(year) {
-  if (year > 0 &&  ) {//heisei
-    return (1988 + year) + "年";
-  } else if (year > 0 && ) { //syouwa
-    return (1925 + year) + "年";
-  } else if (year > 0 && ) {//taisyou
-    return (1912 + year) + "年";
-  } else {
-    return;
-  }
+  //if (year > 0 && year < 28) {
+  //  return (1988 + year) + "年";
+  //} //else {
+  //  return;
+  //}
+
+  var value = year + 1988;
+  return value + "年";
 }
 
 function convertSeirekiToWareki(year) {
-  if (year > 1988) {
-    return "平成" + (year - 1988) + "年";
-  } else if (year > 1925) {
-    return "昭和" + (year - 1925) + "年";
-  } else if (year > 1912) {
-    return "大正" + (year - 1912) + "年";
-  } else {
-    return;
-  }
+
+ // if (year > 1988) {
+ //  return "平成" + (year - 1988) + "年";
+ // } //else if (year > 1925) {
+ //   return "昭和" + (year - 1925) + "年";
+ // } else if (year > 1912) {
+ //   return "大正" + (year - 1912) + "年";
+ // } else {
+ //   return;
+ // }
+
+  var value = year - 1988;
+  return "平成"  + value + "年";
 }
 
-console.log(convertSeirekiToWareki(1988));//平成27年
-//console.log(convertWarekiToSeireki(27));//2015年
+
+console.log(convertWarekiToSeireki(27));//2015年
+console.log(convertSeirekiToWareki(2015));//平成27年
